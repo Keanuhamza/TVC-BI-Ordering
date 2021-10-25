7b38de8d477bedf1542f27d51972af67c403850f
Keanu Hamza <keanuhamza@gmail.com>
2021-10-25 20:43:30 +1100

    FUll of errors

--- src/main/java/com/example/BIService/BIApplication.java
+++ src/main/java/com/example/BIService/BIApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.BIService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BIApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/BIService/services/OrderInteractiveQuery.java
@@ -0,0 +1,50 @@
+package com.example.BIService.services;
+
+import com.example.BIService.*;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class OrderInteractiveQuery {
+
+    private final InteractiveQueryService interactiveQueryService;
+
+    //@Autowired
+    public OrderInteractiveQuery(InteractiveQueryService interactiveQueryService) {
+        this.interactiveQueryService = interactiveQueryService;
+    }
+
+    public long getProductOrderQuantity(String productName) {
+        if (orderStore().get(productName) != null) {
+            return orderStore().get(productName);
+        } else {
+            throw new NoSuchElementException(); //TODO: should use a customised exception.
+        }
+    }
+
+    public List<String> getBrandList() {
+        List<String> brandList = new ArrayList<>();
+        KeyValueIterator<String, Long> all = brandStore().all();
+        while (all.hasNext()) {
+            String next = all.next().key;
+            brandList.add(next);
+        }
+        return brandList;
+    }
+
+
+    private ReadOnlyKeyValueStore<String, Long> brandStore() {
+        return this.interactiveQueryService.getQueryableStore(OrderStreamProcessing.ORDER_STATE_STORE,
+                QueryableStoreTypes.keyValueStore());
+    }
+
+
+
+}
